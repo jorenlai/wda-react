6223766e8869029c4f3be00a43e901f4cfb7c2dd
Joren Lai <joren.lai@gmail.com>
2023-12-28 15:08:44 +0800

    select q

--- src/exercise/administrator/index.jsx
+++ src/exercise/administrator/index.jsx
@@ -155,7 +155,6 @@ const getLeafLength=(question,length,keys)=>{
         return length+1
     }
 }
-const leafLenght=getLeafLength(questionsf,0,[])
 
 const QQuestion=({question})=>{
     return <div>{question?.question}
@@ -164,7 +163,6 @@ const QQuestion=({question})=>{
                 return <QQuestion question={question} key={index}/>
             })
         }
-
     </div>
 }
 
@@ -176,58 +174,16 @@ const ShowQuestion=({questions})=>{
                 return <QQuestion question={question} key={index}/>
             })
         }
-
     </div>
 }
 
 const QuestionPanel=({questions,questionParams})=>{
     const dispatch = useDispatch()
     const administrator = useSelector((state) => state.administrator)
-    let leafIndex=0
 
-    const thisQ=(question,keys)=>{
-        const a= (Array.isArray(question.props.children)
-            ? question.props.children
-            : question.props.children
-                ? [question.props.children]
-                : []
-        )
-        .filter((child)=>[ITitle,IDescription].indexOf(child.type)>-1)
-        .reduce((aco,child) => {
-            aco[child.type.name]=child
-            return aco;
-        },{keys})
-        return a;
-    }
-
-    const selectQues=(question,selectedIndex,keys)=>{
-        const children=Array.from(Array.isArray(question.props.children)
-                ? question.props.children
-                : question.props.children
-                    ? [question.props.children]
-                    : []
-            )
-            .filter((child)=>{
-                return child.type===IQuestion
-            })
-
-        if(children.length>0){
-            for(var i=0;i<children.length;i++){
-                const child=children[i]
-                const result=selectQues(child,selectedIndex,[...keys,i+1])
-                if(result!=null) {
-                    result.push(thisQ(question,keys))
-                    return result
-                }
-            }
-        }else if(++leafIndex===selectedIndex){
-            return [thisQ(question,keys)]
-        }
-    }
-    
     const navQ=(_num)=>{
         const sum=administrator.selectedIndex+_num 
-        const selectedIndex=sum<=1 ? 1:sum//>leafLenght?administrator.selectedIndex:sum
+        const selectedIndex=sum<=1 ? 1:sum>questionParams.length?questionParams.length:sum
         dispatch(administratorActions.setState(
             {
                 selectedIndex
@@ -236,6 +192,12 @@ const QuestionPanel=({questions,questionParams})=>{
         ))
     }   
     
+    const nav=(num)=>{
+        console.clear()
+        po('questionParams',questionParams)
+        po(administrator.selectedIndex)
+    } 
+
     useEffect(()=>{
         if(administrator.selectedIndex==null){
             dispatch(administratorActions.setState(
@@ -247,9 +209,12 @@ const QuestionPanel=({questions,questionParams})=>{
     const selectedQuestion=findQuestion(questionParams?.questions,administrator.selectedIndex)
     return <StyledQuestionPanel>
         <button>Start</button><button>Stop</button><br/>
-        <button onClick={()=>navQ(-1)}>Pre</button>
+        <button onClick={()=>navQ(-1)}>pre</button>
         {administrator.selectedIndex}
-        <button onClick={()=>navQ(1)}>next</button><br/>
+        <button onClick={()=>navQ(1)}>next</button><br/><br/>
+
+        <button onClick={()=>nav(-1)}>pre</button>
+        <button onClick={()=>nav(1)}>next</button><br/>
         <ShowQuestion questions={selectedQuestion!=null?[selectedQuestion]:null}/>
     </StyledQuestionPanel>
 }
