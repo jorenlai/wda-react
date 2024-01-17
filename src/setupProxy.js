73833ab0711e523eb5576b9c5fa154c0d459167b
Joren Lai <joren.lai@gmail.com>
2024-01-17 09:56:55 +0800

    Updates

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = (app) => {
-
-    app.use(createProxyMiddleware('/api', {
-        target: process.env.api_url
-        ,changeOrigin: true
-    }));
-}
-
--- src/web/header.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function Header(){
-    return <div className={'header'}>
-        店鋪後台管理系統
-    </div>
-}
